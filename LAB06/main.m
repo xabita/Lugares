e7d9c5bcc6472ed99b9bb727892d5331db1ab222
xabita <mutskxabaa@gmail.com>
2015-02-13 23:50:06 -0600

    Initial Commit

---
+++ LAB06/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LAB06
+//
+//  Created by Elizabeth Martínez Gómez on 13/02/15.
+//  Copyright (c) 2015 Elizabeth Martínez Gómez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
